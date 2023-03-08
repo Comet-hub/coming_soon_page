549ac64449b10e8dd9b37e2f63a90ea78ce2174f
Camilo Castellanos <icamilocc@gmail.com>
2023-03-07 23:17:29 -0500

    Cambio de formato del archivo js

--- js/script.js
+++ js/script.js
@@ -2,24 +2,23 @@ const validationMge = document.querySelector("#validationMessage");
 const emailForm = document.querySelector("#emailForm");
 const emailAddress = document.querySelector("#emailAddress");
 
-emailAddress.addEventListener("input",(event) => {
+emailAddress.addEventListener("input", (event) => {
   if (emailAddress.validity.valid) {
     validationMge.textContent = "";
-  }else{
+  } else {
     ShowError();
   }
 });
-emailForm.addEventListener("submit",(event) => { 
+emailForm.addEventListener("submit", (event) => {
   if (!emailAddress.validity.valid) {
     ShowError();
     event.preventDefault();
   }
 });
-const ShowError = () => { 
+const ShowError = () => {
   if (emailAddress.value == "") {
     validationMge.textContent = "Please povide a email";
-  } else if(emailAddress.validity.typeMismatch) {
-    validationMge.textContent = "Please povide a valid email"; 
+  } else if (emailAddress.validity.typeMismatch) {
+    validationMge.textContent = "Please povide a valid email";
   }
-}
-
+};
